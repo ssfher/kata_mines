411898371a161d56a2e3f40c4b79f8a7ecbfc6a8
Jonathan Tinajero <jnth_pt@hotmail.com>
2016-10-28 09:38:07 -0500

    Removed underscore

--- client/app/scripts/mines/controller.js
+++ client/app/scripts/mines/controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Minesweeper')
-.controller('minesweeper', function ($scope, _) {
+.controller('minesweeper', function ($scope) {
 
   $scope.controller_loaded = 'Minesweeper loaded!';
 
